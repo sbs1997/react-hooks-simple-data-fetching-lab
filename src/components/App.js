4f1980f549578a6257ea59566ff116cfca4ff24f
Sky Bauerschmidt Sweeney <dbauerschmidtsweeney@gmail.com>
2023-08-23 16:16:13 -0600

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App(){
+    const [searchReturn, setSearchReturn] = useState("Loading")
+    useEffect(
+        ()=>{
+            fetch('https://dog.ceo/api/breeds/image/random')
+            .then(r=>r.json())
+            .then((dog)=>{
+                setSearchReturn(dog.message)
+            })
+        }, []
+    )
+    return(
+        <>
+            {searchReturn === "Loading" ?
+                <p>Loading...</p>
+                :
+                <img src={searchReturn} alt="A Random Dog"/>
+            }
+        </>
+    )
+}
+
+export default App
